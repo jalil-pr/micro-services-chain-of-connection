2660f5b4f91454aad5e8eef9d2c2ac3aff3ece35
Jalil <jalilhaidari53@gmail.com>
2023-01-08 11:55:50 +0430

    initial setup

---
+++ cloud-config-server-for-chain-of-connections/src/main/java/com/chainofconnection/cloudconfigserverforchainofconnections/CloudConfigServerForChainOfConnectionsApplication.java
@@ -0,0 +1,13 @@
+package com.chainofconnection.cloudconfigserverforchainofconnections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigServerForChainOfConnectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerForChainOfConnectionsApplication.class, args);
+	}
+
+}
---
+++ cloud-config-server-for-chain-of-connections/src/test/java/com/chainofconnection/cloudconfigserverforchainofconnections/CloudConfigServerForChainOfConnectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chainofconnection.cloudconfigserverforchainofconnections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerForChainOfConnectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
